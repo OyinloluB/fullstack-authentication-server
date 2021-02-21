4bdeb9c23b195c8a789634da03137fa052fb16ed
OyinloluB <oyinloluwab@gmail.com>
2021-02-21 20:49:53 +0100

    passport setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const passport = require("passport");
 
 const app = express();
+
+app.use(passport.initialize());
 
 app.listen(5000, () => {
   console.log("now listening for requests on port 5000");
